308f958ad727a21a3aad41230e2142dfd3a21f6e
songhyoenjin <96371441+songhyoenjin@users.noreply.github.com>
2021-12-19 17:50:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,11 @@ const CONFIG = {
     btnIntro: 'Em biết anh đang rất cố gắng để tốt hơn Thương anh , yên tâm anh nhé , em vẫn ở phía sau anh',
     title: 'em chưa từng hối hận khi thương anh hết  😙',
     desc: 'cảm ơn anh vì đã cho e khoảng thời gian đẹp ngày qua a nhé !  ',
-    btnYes: 'e cũng thế ',
-    btnNo: 'e thấy lạ lắm , cảm giác khó tả ',
+    btnYes: 'a cũng thế ',
+    btnNo: 'a thấy lạ lắm , cảm giác khó tả ',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho em <3',
     reply: ' dạ tí anh trl nhó <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
